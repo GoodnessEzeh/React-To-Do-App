03c641017b967be1c6bc302a1fa218bb3b44373b
Goodness Ezeh <ezehgoodness@gmail.com>
2021-10-25 21:07:42 +0900

    First Commit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+    
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+    inputRef.current.focus();
+    });
 
     const handleChange = e => {
         setInput(e.target.value);
@@ -19,18 +25,37 @@ function TodoForm(props) {
     
     return (
         <form className='todo-form' onSubmit={handleSubmit}>
+          {props.edit ? (
+        <>
           <input
-            type='text'
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
             placeholder='Add a todo'
             value={input}
+            onChange={handleChange}
             name='text'
             className='todo-input'
-            onChange={handleChange}
+            ref={inputRef}
           />
-          <button className='todo-button'>Add todo
+          <button onClick={handleSubmit} className='todo-button'>
+            Add todo
           </button>
-        </form>
-    );
+        </>
+      )}
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
